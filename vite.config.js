cbb992f73ec29a8ec8d3c81228baa63dce5ea1e0
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2025-06-10 17:46:17 +0000

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import {
-    defineConfig
-} from 'vite'
+// vite.config.js (en la raíz de tu repositorio)
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -9,6 +8,8 @@ export default defineConfig({
         port: 3000
     },
     build: {
-        outDir: 'dist'
-    }
+        outDir: 'dist' // Esto ya está bien, construirá en [repo_root]/dist
+    },
+    root: 'src/front' // <--- ¡Añade o modifica esta línea!
+                      // Le dice a Vite que el código fuente de tu app está aquí
 })
